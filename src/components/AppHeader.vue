696b6a441aeaffa3b49068a8fd1bfc23b168e308
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-04-08 13:03:05 +0200

    add Header Component Commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,33 +1,58 @@
-
 <script>
+import {headerInfoState} from "../store/headerInfoState"
 export default {
     name: "AppHeader",
     data() {
         return {
-            
+            headerInfoState,
+            hInfo: headerInfoState.headerInfo
             /* ... */
         }
+    },
+    mounted() {
+        console.log(this.hInfo)
     }
 }
 </script>
 <template>
     <div id="site_header">
         <div class="header_container">
-            <ul class="header_list">
-                <li> <img src="../assets/img/" alt=""></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <!-- /.header_list -->
+            <div class="header_item" v-for="headerItem in hInfo">
+                <div>
+                    <img class="logo" :src="`../src/assets/logos/${headerItem.info.logoHeader_path}`" alt="Logo">
+                </div>
+                <div class="header_info">
+                    <div>
+                        {{ headerItem.info.homeString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.aboutString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.vehiclesString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.dealersString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.packagesString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.blogString }}
+                    </div>
+                    <div>
+                        {{ headerItem.info.contactString }}
+                    </div>
+                    <div class="actions">
+                        <a class="cta" href="#">{{ headerItem.actions.myAccount }}</a>
+                        <a class="cta" href="#">{{ headerItem.actions.addCar }}</a>
+                        <i class="fa-solid fa-bars"></i>
+                        <a class="cta" href="#">{{ headerItem.actions.phoneNumber }}</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.header_item -->
+
         </div>
         <!-- /.header_container -->
     </div>
@@ -37,10 +62,40 @@ export default {
 
 <style scoped>
 #site_header{
-    background-color: var(--lighter-color);
+    background-color: var(--dark-color);
     width: 100vw;
-    height: 100px;
+    height: 50px;
     position: fixed;
-}
+    display: flex;
+    >.header_container{
+        width: 60%;
+        margin: auto;
+        >.header_item{
+            display: flex;
+            justify-content: space-around;
+            .logo{
+                height: 50px;
+            };
+            >.header_info{
+               display: flex;
+               align-items: center;
+               font-size: 0.8rem;
+               margin-bottom: 10px;
+               gap: 1.2rem;
+               .actions{
+                display: flex;
+                align-items: center;
+                gap: 1rem;
+               .cta{
+                background-color: var(--darkest-color);
+                padding: 0.5rem;
+                text-decoration: none;
+                color: var(--lighter-color)
+                    }
+                };
+            };
+        };
+    };
+};
 
 </style>
--- src/store/headerInfoState.js
+++ src/store/headerInfoState.js
@@ -1,24 +1,24 @@
 import { reactive } from "vue";
 export const headerInfoState = reactive({
-  headerInfo: {
-    logoHeader: logoHeader_path,
-    logoFooter: logoFooter_path,
-    info: [
-      { homeString: "home" },
-      { aboutString: "About Us" },
-      { vehiclesString: "Vehicles" },
-      { dealersString: "Dealers" },
-      { packagesString: "Packages" },
-      { blogString: "Blog" },
-      { contactString: "Contact" },
-    ],
-    actions: [
-      { myAccount: "My Account" },
-      { addCar: "Add Car" },
-      { phoneNumber: "+1 (234) 567 89 10" },
-      { Menu: '<i class="fa-solid fa-bars"></i>' },
-    ],
-  },
-  logoHeader_path: "/logo-autocar11.png",
-  logoFooter_path: "/logo-footer-autocar11.png",
+  headerInfo: [
+    {
+      info: {
+        logoHeader_path: "logo-autocar11.png",
+        logoFooter_path: "logo-footer-autocar11.png",
+        homeString: "Home",
+        aboutString: "About Us",
+        vehiclesString: "Vehicles",
+        dealersString: "Dealers",
+        packagesString: "Packages",
+        blogString: "Blog",
+        contactString: "Contact",
+      },
+      actions: {
+        myAccount: "My Account",
+        addCar: "Add Car",
+        phoneNumber: "+1 (234) 567 89 10",
+        menu: '<i class="fa-solid fa-bars"></i>',
+      },
+    },
+  ],
 });
